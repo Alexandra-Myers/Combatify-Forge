ba515f39286d6003e13b40e81c88654da27ae14f
Alexandra-Myers <bia416cat@gmail.com>
2023-05-25 16:16:05 -0400

    ditto of 1.1.5

--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/MilkBucketMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/MilkBucketMixin.java
@@ -4,20 +4,15 @@ import net.alexandra.atlas.atlas_combat.AtlasCombat;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.MilkBucketItem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Unique;
-
-import java.util.Collections;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MilkBucketItem.class)
 public class MilkBucketMixin {
 
-	/**
-	 * @author zOnlyKroks
-	 * @reason because
-	 */
-	@Overwrite
-	public int getUseDuration(ItemStack stack) {
-		return AtlasCombat.CONFIG.milkBucketUseDuration.get();
+	@Inject(method = "getUseDuration", at = @At(value = "RETURN"), cancellable = true)
+	public void getUseDuration(ItemStack itemStack, CallbackInfoReturnable<Integer> cir) {
+		cir.setReturnValue(AtlasCombat.CONFIG.milkBucketUseDuration.get());
 	}
 }
