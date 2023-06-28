c1c711194a600875e00113049e05852d4b972583
Alexandra-Myers <bia416cat@gmail.com>
2023-06-28 18:29:25 -0400

    forge changes

--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/DiggerItemMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/DiggerItemMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,6 +26,8 @@ public abstract class DiggerItemMixin extends TieredItem implements Vanishable, 
 	@Mutable
 	private WeaponType type;
 	@Shadow
+	@Mutable
+	@Final
 	private Multimap<Attribute, AttributeModifier> defaultModifiers;
 	public DiggerItemMixin(Tier tier, Properties properties) {
 		super(tier, properties);
--- src/main/java/net/alexandra/atlas/atlas_combat/mixin/SwordItemMixin.java
+++ src/main/java/net/alexandra/atlas/atlas_combat/mixin/SwordItemMixin.java
@@ -22,7 +22,9 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.ToolAction;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -34,6 +36,8 @@ import java.util.List;
 @Mixin(SwordItem.class)
 public class SwordItemMixin extends TieredItem implements ItemExtensions, IShieldItem, ISwordItem, DefaultedItemExtensions, WeaponWithType {
 	@Shadow
+	@Mutable
+	@Final
 	private Multimap<Attribute, AttributeModifier> defaultModifiers;
 	public int strengthTimer = 0;
 
